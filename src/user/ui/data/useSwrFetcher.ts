9ef7d586af51227d64daf52e8dac07a287d78145
Gabriel Santos <gabriel@ogabrielsantos.com.br>
2023-06-15 23:04:23 -0300

    Add swr fetcher

---
+++ src/user/ui/data/useSwrFetcher.ts
@@ -0,0 +1,20 @@
+import useSWR from 'swr';
+import {
+  type FetcherPromise,
+  type FetcherResponse,
+} from '../../domain/data/fetcherInterface';
+
+function useSwrFetcher<T>(
+  key: string | null,
+  promise: FetcherPromise<T>,
+): FetcherResponse<T> {
+  const { data, error, isLoading } = useSWR(key, promise);
+
+  return {
+    data,
+    error,
+    isLoading,
+  };
+}
+
+export default useSwrFetcher;
