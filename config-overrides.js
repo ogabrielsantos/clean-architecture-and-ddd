0987a9e14ba5b5fe7e3ac5a6262495a1a0f67692
Gabriel Santos <gabriel@ogabrielsantos.com.br>
2023-06-15 22:57:53 -0300

    Add alias to user as @user

---
+++ config-overrides.js
@@ -0,0 +1,9 @@
+const {
+  aliasWebpack,
+  aliasJest,
+} = require('react-app-alias-ex');
+
+const options = {};
+
+module.exports = aliasWebpack(options);
+module.exports.jest = aliasJest(options);
